61e75b84cc9e918b7d3b40a2290dfae72a865a4c
howard wu <dothewu@gmail.com>
2020-03-03 00:59:36 +0000

    Initial commit

---
+++ src/main/java/com/tacoloco/pricingCalculator/PricingCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.tacoloco.pricingCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tacoloco/princingCalculator/PricingCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacoloco.pricingCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
